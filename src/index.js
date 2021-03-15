a1e3c8596afbab68f79f3d6dfdffcb9c7372d844
gabdykalyl <gabdykalyk@gmail.com>
2021-03-15 15:17:06 +0600

    feat: task finished

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined) {
+        return 0;
+    }
+    else if (array.length > 0)  {
+        let minNum = array[0]
+        for (let i = 0; i < array.length; i++) {
+            if (minNum > array[i]) {
+                minNum = array[i]
+            }
+        }
+        return minNum
+    }
+    else {
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined) {
+        return 0;
+    }
+    else if  (array.length > 0) {
+        let maxNum = array[0]
+        for (let i = 0; i < array.length; i++) {
+            if (maxNum < array[i]) {
+                maxNum = array[i]
+            }
+        }
+        return maxNum
+    }
+    else {
+        return 0;
+
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined) {
+        return 0;
+    }
+    else if (array.length > 0) {
+        let sum = 0
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i]
+        }
+        return sum / array.length
+    }
+    else {
+        return 0;
+    }
 }
